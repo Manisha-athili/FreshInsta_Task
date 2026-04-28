b12f9087f3029136045bad0f8805cb29f5f9ed03
Manisha_athili <manishaathili1126@gmail.com>
2026-04-28 20:55:25 +0530

    manisha/Feat-added rate-limiting

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
+import rateLimit from "express-rate-limit";
+import morgan from "morgan";
 
 const app = express();
 app.use(express.json());
@@ -8,10 +10,13 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan("dev"));
 
-app.use(rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100
-}));
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 5
+});
+
+
+app.use(limiter);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
