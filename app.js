758dd4aa3cf654c411340398593ef7ec99850fef
Manisha_athili <manishaathili1126@gmail.com>
2026-04-28 20:54:56 +0530

    manisha/Feat-added rate-limiting

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ import taskRoutes from "./routes/taskRoutes.js";
 
 const app = express();
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
